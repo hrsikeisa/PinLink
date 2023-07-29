6373fa8bfe7b19340b521b2615da8676dd401f47
2hari <two1821125run@protonmail.com>
2023-07-29 18:12:03 -0400

    specify language in document.tsx

--- consts/landingpage.ts
+++ consts/landingpage.ts
@@ -3,4 +3,5 @@ export const USERS = [
     username: 'theo',
     pfp: 'https://res.cloudinary.com/harihari/image/upload/v1694395723/apps/pinlink-web/fluyaavplicw081ay519.jpg',
   }
+  // todo add more
 ]
